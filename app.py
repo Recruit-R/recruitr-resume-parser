598523befc2930a07706c3b0b3ff28e46cf8b5cd
avaHatfield <106991181+avaHatfield@users.noreply.github.com>
2024-04-02 18:25:45 -0400

    a bunch of stuff

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask
+from pyresparser import ResumeParser
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello world</p>"
+@app.route("/testlocal")
+def test_local():
+    data = ResumeParser('test_resume.pdf').get_extracted_data()
+    return data    
