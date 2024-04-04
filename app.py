94cbec5eb98c933305cfdfd3a15adfb583aaafa0
avaHatfield <106991181+avaHatfield@users.noreply.github.com>
2024-04-04 10:19:23 -0400

    main fun

--- app.py
+++ app.py
@@ -10,3 +10,26 @@ def hello_world():
 def test_local():
     data = ResumeParser('test_resume.pdf').get_extracted_data()
     return data    
+
+#for cloud function
+#@functions_framework.http
+def parse_resume(request):
+    print(request)
+    try:
+        data = request.json.get('data')
+        if data:
+            print(data)
+            pdf_bytes_io = io.BytesIO(base64.b64decode(data))
+            pdf_bytes_io.name = "Resume.pdf"
+            return_data = ResumeParser(pdf_bytes_io).get_extracted_data()
+            return return_data
+        else:
+            return {
+            'success': False,
+            'message': 'No data found in the request'
+            }
+    except Exception as e:
+        return {
+        'success': False,
+        'message': str(e)
+        }
