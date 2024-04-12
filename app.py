9592b40e26300e2c340c7ac608ebf5aec784784b
TheDeafOne <Woodburnkb20@gcc.edu>
2024-04-11 20:04:06 -0400

    added cors, removed print statements, updated requirements.txt

--- app.py
+++ app.py
@@ -1,20 +1,22 @@
-from flask import Flask
-from flask import request
-from pyresparser import ResumeParser
 import base64
 import io
 import os
 
+from flask import Flask, request
+from flask_cors import CORS
+from pyresparser import ResumeParser
+
 app = Flask(__name__)
+CORS(app)
+
 @app.route("/", methods=['POST'])
 def parse_resume():
-    print(request)
     try:
         data = request.json.get('data')
         if data:
-            print(data)
             pdf_bytes_io = io.BytesIO(base64.b64decode(data))
             pdf_bytes_io.name = "Resume.pdf"
+
             return_data = ResumeParser(pdf_bytes_io).get_extracted_data()
             return return_data
         else:
@@ -27,6 +29,8 @@ def parse_resume():
             'success': False,
             'message': str(e)
         }
+    
+
 if __name__ == '__main__':
     # Get the port from the environment variable if available, otherwise use 8080
     port = int(os.environ.get('PORT', 8080))
